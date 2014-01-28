19ab68b96695f507a57e92f338d871d5896a51c5
Everett <everett@mentaltangent.net>
2014-01-27 16:06:18 -0800

    Complete beta version of PSE IG

--- projects/PSE/pseig.php
+++ projects/PSE/pseig.php
@@ -6,58 +6,90 @@ KIGProjectHeader();
 
 ?>
 
+<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed ornare velit enim, sed pulvinar nulla euismod hendrerit. Nunc auctor hendrerit turpis, sit amet dignissim risus vulputate non. Cras sodales tellus ac viverra semper. Donec vitae ligula luctus, aliquam libero nec, congue tortor. Vestibulum et blandit lacus. Nunc gravida felis in erat elementum placerat. Aliquam justo felis, tincidunt ut erat mattis, ullamcorper lobortis dui. Mauris venenatis lorem et eros rutrum, sed pulvinar lacus pellentesque. Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+
+<p>Aliquam quis elit sit amet purus fringilla consectetur tincidunt a tortor. Phasellus tincidunt lectus ut tempor tempor. Ut eu consequat mauris. Nunc convallis magna aliquet, dictum massa eget, varius orci. Aliquam venenatis quam eu dolor euismod pellentesque. Sed eu libero et nisl interdum cursus. Aliquam laoreet in diam et tristique. Ut ipsum dolor, aliquam in porttitor vitae, vestibulum sit amet tortor. Maecenas in rhoncus nibh. Duis dui ante, facilisis in tincidunt non, lacinia in nunc. Morbi vel leo convallis, tincidunt odio ac, luctus ligula. Vivamus sed tellus aliquam, commodo dui et, dictum massa.</p>
+
+<p>Vestibulum sit amet rutrum eros. Cras suscipit justo ut neque egestas, et vehicula justo tincidunt. Quisque sagittis dui vel purus tempus imperdiet. Duis leo nisl, gravida et porta ac, fringilla a lectus. Praesent vehicula nulla non nunc semper, eget auctor ligula volutpat. Cras vel facilisis velit. Suspendisse erat arcu, laoreet at velit a, aliquet interdum erat. Donec nulla nulla, euismod non quam sed, laoreet vestibulum sapien. Etiam nulla est, consectetur ut nunc et, vehicula rutrum risus. Aliquam imperdiet congue purus sed semper. Phasellus vulputate pharetra odio ut pulvinar. Phasellus sed facilisis purus. Integer hendrerit dui dignissim, tempor libero nec, porttitor eros. Suspendisse purus leo, faucibus quis nunc et, eleifend ultrices neque. Vivamus tincidunt arcu vitae velit placerat congue.</p>
+
+<p>Proin venenatis justo vel lorem fermentum rutrum. Sed volutpat lorem porta lorem faucibus suscipit. Vestibulum lobortis commodo massa ac sagittis. Donec eget hendrerit arcu. Morbi nisi ante, imperdiet quis hendrerit vel, tempor vitae elit. Curabitur vitae tellus id lacus semper mollis. Nam massa diam, interdum in dolor cursus, luctus scelerisque velit. Etiam interdum mauris et adipiscing convallis. Phasellus quis sagittis leo. Curabitur volutpat nisl urna, molestie sollicitudin nunc feugiat nec. Curabitur cursus felis sed sapien mollis dictum.</p>
+
+<p>Suspendisse semper massa aliquet arcu condimentum cursus. Quisque nec ultricies tortor. Vivamus mollis turpis eu pellentesque consectetur. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Phasellus commodo risus nec nulla cursus molestie. Mauris quis leo placerat, varius ligula ut, ullamcorper est. Vivamus vitae lacus eu nisl tincidunt accumsan vel vel enim. Ut semper, leo non varius dignissim, ante lorem porta turpis, nec malesuada lacus mi ut nibh. Nulla facilisi. Nullam bibendum eget mauris ac blandit. Integer in ultricies tortor. In tempor placerat sem, nec hendrerit ligula pellentesque a. Praesent fermentum neque dictum orci tincidunt lobortis.</p>
+
+<p>Nullam id venenatis lacus, eget convallis diam. Proin vel posuere ligula. Mauris aliquam arcu et quam sagittis volutpat non ac ligula. Curabitur mollis dolor sed sem ullamcorper accumsan nec vitae nisi. Etiam pharetra ante mauris, id egestas justo porttitor dictum. Mauris posuere pharetra velit nec facilisis. Cras ut fringilla felis, eget pellentesque elit. Sed non euismod arcu. Etiam arcu eros, tincidunt at semper eget, condimentum quis orci. Integer viverra tempus tellus vel adipiscing. Phasellus tempus rutrum nisl, nec tempor diam. Praesent venenatis bibendum augue, ut euismod lectus. Integer porttitor mauris ut sodales interdum. Vestibulum gravida vel ipsum vel rutrum. Nunc gravida magna a condimentum consequat.</p>
+
+<p>Aliquam mauris purus, pretium id odio vel, condimentum tristique metus. Mauris et gravida turpis. Nullam faucibus libero ut justo commodo mollis. Praesent pulvinar erat aliquet, consequat ipsum a, varius erat. Nulla varius in enim a dignissim. Integer luctus ipsum est, id congue diam ultrices vitae. In velit dolor, ultrices a leo a, congue porta mi.</p>
+
+<p>Vivamus volutpat, mi at ultricies molestie, augue enim varius orci, id mattis urna dui eget ligula. Maecenas in purus ultricies, sodales justo ut, molestie magna. Etiam adipiscing vitae tellus quis tincidunt. Suspendisse venenatis dui risus, at molestie lorem interdum nec. Aenean adipiscing felis et est semper, sed molestie est interdum. Mauris eget sem est. Quisque commodo risus nec velit egestas fermentum sit amet et leo.</p>
+
+<p>Donec et ipsum sit amet ligula aliquam gravida. Morbi tortor nunc, malesuada eu risus hendrerit, gravida tempor eros. Aenean dictum felis eu mauris tincidunt, vel dapibus lectus venenatis. Nam felis velit, sodales sit amet vehicula tincidunt, porta vel magna. Etiam feugiat ut sem sit amet commodo. Donec gravida et eros at fermentum. Phasellus et vulputate est. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae;</p>
+
+<p>Nulla non turpis a risus pharetra varius. Fusce mollis, magna ac volutpat interdum, nisi nibh tincidunt sem, sit amet tincidunt sem urna sit amet eros. Aliquam suscipit bibendum felis ut fringilla. Donec posuere orci eleifend odio fringilla, tristique tempus orci euismod. Maecenas commodo sed mi pretium lacinia. Curabitur vel dolor accumsan, feugiat velit a, lacinia turpis. Ut vitae tristique dui. Donec eget tincidunt enim. Proin elit magna, suscipit nec euismod ac, facilisis eu dui.</p>
+
 <!-- Body stuff -->
 <div id="box" class="bounding-box">
+	<div id="expander">
+		<div class="row">
+			<div class="col-xs-8 col-md-12">
+				<img src="img/text_main.svg" class="img-responsive title-img">
+			</div>
+		</div>
+		<div class="row">
+			<div class="col-xs-8 col-md-12">
+				<img src="img/text_save.svg" class="img-responsive title-img">
+			</div>
+		</div>
+		<div class="row">
+			<div class="col-xs-offset-4 col-xs-3 col-sm-2 col-sm-offset-5 tips-icon">
+				<a href="#" data-toggle="modal" data-target="#myModal" onclick="tips()"><img src="img/icon_tips.svg" class="img-responsive animated pulse"></a>
+			</div>
+		</div>
+		<img src="img/cycle.svg" class="img-responsive focus-image">
+		<div class="row img-lift">
+			<div class="col-xs-4 col-xs-offset-1 col-sm-4 col-sm-offset-1 bump-right">
+				<a href="#" data-toggle="modal" data-target="#myModal" onclick="renewable()"><img src="img/icon_renewable.svg" class="img-responsive primary-image animated pulse"></a>
+			</div>
+			<div class="col-xs-4 col-xs-offset-1 col-sm-4 col-sm-offset-2 bump-right">
+				<a href="#" data-toggle="modal" data-target="#myModal" onclick="wind()"><img src="img/icon_wind.svg" class="img-responsive primary-image animated pulse"></a>
+			</div>
+		</div>
+		<div class="row spacer-row">
+			<div class="col-xs-offset-4 col-xs-3 col-md-offset-5 col-md-2">
+				<p id="spacer-text" class="cta-text animated">Click each icon to see how PSE helps you save!</p>
+			</div>
+		</div>
+		<div class="row">
+			<div class="col-xs-4 col-xs-offset-1 col-sm-4 col-sm-offset-1 bump-right">
+				<a href="#" data-toggle="modal" data-target="#myModal" onclick="hydro()"><img src="img/icon_hydroelectric.svg" class="img-responsive primary-image animated pulse"></a>
+			</div>
+			<div class="col-xs-4 col-xs-offset-1 col-sm-4 col-sm-offset-2 bump-right">
+				<a href="#" data-toggle="modal" data-target="#myModal" onclick="efficiency()"><img src="img/icon_efficiency.svg" class="img-responsive primary-image animated pulse"></a>
+			</div>
+		</div>
+	</div> <!-- /expander -->
 	<div class="row">
-		<img src="img/text_main.svg" class="img-responsive title-img">
-	</div>
-	<div class="row">
-		<img src="img/text_save.svg" class="img-responsive title-img">
-	</div>
-	<!-- <div class="row">
-		<div class="col-md-10">
-			<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.</p>
-		</div>
-		<div class="col-md-2">
-			<a href="#" data-toggle="modal" data-target="#myModal" onclick="tips()"><img src="http://placehold.it/100&text=tips" class="img-responsive img-circle"></a>
-		</div>
-	</div> 
-	<div class="row">
-		<div class="col-md-12">
-			<h2 class="text-center" style="margin-bottom:50px;">These are the things we invest in!</h2>
-		</div>
-	</div> -->
-	<div class="row">
-		<div class="col-sm-offset-5 col-sm-2 tips-icon">
-			<a href="#" data-toggle="modal" data-target="#myModal" onclick="tips()"><img src="img/icon_tips.svg" class="img-responsive"></a>
-		</div>
-	</div>
-	<img src="img/cycle.svg" class="img-responsive focus-image">
-	<div class="row img-lift">
-		<div class="col-md-4 col-sm-offset-1">
-			<a href="#" data-toggle="modal" data-target="#myModal" onclick="renewable()"><img src="img/icon_renewable.svg" class="img-responsive img-circle primary-image"></a>
-		</div>
-		<div class="col-md-4 col-sm-offset-2">
-			<a href="#" data-toggle="modal" data-target="#myModal" onclick="wind()"><img src="img/icon_wind.svg" class="img-responsive img-circle primary-image"></a>
-		</div>
-	</div>
-	<div class="spacer-row" style="height:270px;">
-	</div>
-	<div class="row">
-		<div class="col-md-4 col-md-offset-1">
-			<a href="#" data-toggle="modal" data-target="#myModal" onclick="hydro()"><img src="img/icon_hydroelectric.svg" class="img-responsive img-circle primary-image"></a>
-		</div>
-		<div class="col-md-4 col-md-offset-2">
-			<a href="#" data-toggle="modal" data-target="#myModal" onclick="efficiency()"><img src="img/icon_efficiency.svg" class="img-responsive img-circle primary-image"></a>
-		</div>
-	</div>
-	<div class="row">
-		<div class="col-md-12">
+		<div class="col-xs-8 col-md-12">
 			<img src="img/text_conserve.svg" class="img-responsive">
 		</div>
 	</div>
 	<div class="row">
-		<img src="img/text_mypse.svg" class="pull-right img-responsive">
+		<div class="col-xs-3 col-xs-offset-1 col-sm-3 col-sm-offset-1 col-md-3 col-md-offset-1">
+			<img src="img/pse-twopeople.svg" class="img-responsive">
+		</div>
+		<div class="col-xs-3 col-sm-3 col-md-3">
+			<img id="event-button" src="img/pse-onyourside.svg" class="img-responsive animated pulse hidden link-bubble">
+		</div>
+		<div class="col-xs-3 col-sm-3 col-md-3">
+			<img src="img/pse-pointingginger.svg" class="img-responsive">
+		</div>
+		<div class="col-xs-2 col-sm-2 col-md-2">
+			<img id="chat-bubble" src="img/pse-bubble.svg" class="img-responsive animated hidden chat-bubble">
+		</div>
+	</div>
+	<div class="row">
+		<div class="col-xs-2 col-xs-offset-8 col-md-4 col-md-offset-8">
+			<img src="img/text_mypse.svg" class="img-responsive pull-right">
+		</div>
 	</div>
 </div>
 
@@ -70,14 +102,15 @@ KIGProjectHeader();
 				<button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
 			</div> -->
 			<div class="modal-body">
-				<div id="modal-content" class="row">
-					<div class="col-md-1">
-						<button id="btn-left" class="btn btn-default lower pull-left" type="button" onclick="left()" style="margin-top: 128.5px;"><span class="glyphicon glyphicon-chevron-left"></span></button>
-					</div>
-					<div id="modal_svg" class="col-md-10">
-					</div>
-					<div class="col-md-1">
-						<button id="btn-right" class="btn btn-default lower pull-right" type="button" onclick="right()" style="margin-top: 128.5px;"><span class="glyphicon glyphicon-chevron-right"></span></button>
+				<div id="modal-content">
+					<div class="row">
+						<div class="col-xs-1 col-sm-1 col-md-1">
+							<button id="btn-left" class="btn btn-default pull-left" type="button" onclick="left()"><span class="glyphicon glyphicon-chevron-left"></span></button>
+						</div>
+						<div id="modal_svg" data-dismiss="modal" class="col-xs-10 col-sm-10 col-md-10"></div>
+						<div class="col-xs-1 col-sm-1 col-md-1">
+							<button id="btn-right" class="btn btn-default pull-right" type="button" onclick="right()"><span class="glyphicon glyphicon-chevron-right"></span></button>
+						</div>
 					</div>
 				</div>
 			</div>
@@ -108,77 +141,7 @@ KIGProjectHeader();
 	</div>
 </div>
 
-
-<script type="text/javascript">
-var currItr = 0, // Figures out which part of the modal content to present
-	currCont, // Figures out which modal content to present
-	content = [];
-
-$(document).ready(function() {
-
-	// Add all of the modal content to content
-	var mod_info = $('.modal-info');
-	mod_info.children().each(function() {
-		var _context = $(this);
-		var _content = _context.children();
-		content.push(_content);
-	});
-
-});
-
-function renewable() {currCont=0; currItr=0; loadCont();}
-function wind() {currCont=1; currItr=0; loadCont();}
-function hydro() {currCont=2; currItr=0; loadCont();}
-function efficiency() {currCont=3; currItr=0; loadCont();}
-function tips() {currCont=4; currItr=0; loadCont();}
-
-function right() {
-	currItr++;
-	if (currItr > (content[currCont].length - 1))
-		currItr = 0;
-	console.log("Right: " +  currItr);
-	loadCont();
-}
-
-function left() {
-	currItr--;
-	if (currItr < 0)
-		currItr = content[currCont].length - 1;
-	console.log("Left: " +  currItr);
-	loadCont();
-}
-
-function loadCont() {
-	if(content[currCont].length != 1) {
-		$('#btn-right').css('display', 'inherit');
-		$('#btn-left').css('display', 'inherit');
-	} else {
-		$('#btn-right').css('display', 'none');
-		$('#btn-left').css('display', 'none');
-	}
-	$('#modal_svg').children().remove();
-	$('#modal_svg').append(content[currCont][currItr]);
-
-	setButtonHeight();
-}
-
-function setButtonHeight() {
-	var h = $('#modal-content').height() * .5 - 10;
-
-	if (h < 0)
-		setTimeout(setButtonHeight, 200);
-	else if(currItr == 5) {
-		// super hacky
-		$('#btn-left').css('margin-top', 60);
-		$('#btn-right').css('margin-top', 60);
-	}
-	else {
-		$('#btn-left').css('margin-top', h);
-		$('#btn-right').css('margin-top', h);
-	}
-}
-
-</script>
+<script src="js/general.js"></script>
 
 <?php
 
--- projects/settings.php
+++ projects/settings.php
@@ -5,10 +5,12 @@ function KIGProjectHeader() {
 	print "<html lang=\"en\">\n";
 	print "<head>\n";
 	print "\t<meta charset=\"UTF-8\">\n";
+	print "\t<meta name=\"viewport\" content=\"width=device-width, initial-scale=1.0\">\n";
 	print "\t<title>KIG Prototype</title>\n";
 	print "\t<link rel=\"stylesheet\" type=\"text/css\" href=\"../../css/bootstrap.css\">\n";
 	print "\t<link rel=\"stylesheet\" type=\"text/css\" href=\"css/custom.css\">\n";
 	print "\t<script src=\"http://code.jquery.com/jquery-1.10.1.min.js\"></script>\n";
+	// print "\t <script src=\"//ajax.googleapis.com/ajax/libs/jqueryui/1.10.3/jquery-ui.min.js\"></script>\n";
 	print "</head>\n";
 	print "<body>\n";
 	print "\t<div class=\"container\">\n";
