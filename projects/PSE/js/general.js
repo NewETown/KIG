c97f1ce8ffad7c73bd738b79d30e8ef4e163ea1e
Everett <everett@mentaltangent.net>
2014-01-29 10:13:16 -0800

    Final push before creating dev branch

--- projects/PSE/js/general.js
+++ projects/PSE/js/general.js
@@ -1,6 +1,7 @@
 var currItr = 0, // Figures out which part of the modal content to present
 	currCont, // Figures out which modal content to present
 	faded = 0,
+	tripwire = ($(window).height() > 0) ? tripwire = $(window).height() : tripwire = 400, // Initialize for safety
 	content = [];
 
 $(document).ready(function() {
@@ -14,27 +15,34 @@ $(document).ready(function() {
 	});
 
 	$( "#event-button" ).click(function() {
-		$( "#expander" ).slideToggle(function() {
-			console.log('Clicked');
-			$('#spacer-text').addClass('fade-in');
-		});
+		$( "#expander" ).slideToggle();
 	});
 
+	$('.hidden').removeClass("hidden");
+	setTimeout(function() {
+		$('#event-button').removeClass("fade-in");
+	}, 1000);
+
+	// var _d = $('#people-row').offset();
+	// var _t = $('#people-row').height() * 1.7;
+	// if (_t < 400)
+	// 	_t = 225;
+	// tripwire = _d.top - _t;
+
 });
 
-$(window).scroll(function() {
+// $(window).scroll(function() {
 
-	_scroll = $(window).scrollTop();
+// 	// _scroll = $(window).scrollTop();
 
-	if (_scroll > 300 && faded == 0) {
-		console.log('Applied');
-		faded = 1;
-		$('.hidden').addClass("fade-in").removeClass("hidden");
-		setTimeout(function() {
-			$('#event-button').removeClass("fade-in");
-		}, 1000);
-	}
-});
+// 	// if (_scroll > tripwire && faded == 0) {
+// 	// 	faded = 1;
+// 	// 	$('.hidden').addClass("fade-in").removeClass("hidden");
+// 	// 	setTimeout(function() {
+// 	// 		$('#event-button').removeClass("fade-in");
+// 	// 	}, 1000);
+// 	// }
+// });
 
 function renewable() {currCont=0; currItr=0; loadCont();}
 function wind() {currCont=1; currItr=0; loadCont();}
@@ -69,7 +77,7 @@ function loadCont() {
 	$('.modal_img_container').children().remove();
 	$('.modal_img_container').append(content[currCont][currItr]);
 
-	setButtonHeight();
+	//setButtonHeight();
 }
 
 function setButtonHeight() {
