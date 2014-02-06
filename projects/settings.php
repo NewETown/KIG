c802f794086bd2c66864206239b594229f0d7c79
Everett <everett@mentaltangent.net>
2014-02-06 13:55:50 -0800

    Finally made progress on the EDC hover animations

--- projects/settings.php
+++ projects/settings.php
@@ -6,19 +6,36 @@ function KIGProjectHeader() {
 	print "<head>\n";
 	print "\t<meta charset=\"UTF-8\">\n";
 	print "\t<meta name=\"viewport\" content=\"width=device-width, initial-scale=1.0\">\n";
-	print "\t<title>KIG Prototype</title>\n";
+	print "\t<title>KIG Prototype Page</title>\n";
 	print "\t<link rel=\"stylesheet\" type=\"text/css\" href=\"../../css/bootstrap.css\">\n";
 	print "\t<link rel=\"stylesheet\" type=\"text/css\" href=\"css/custom.css\">\n";
 	print "\t<script src=\"http://code.jquery.com/jquery-1.10.1.min.js\"></script>\n";
-	// print "\t <script src=\"//ajax.googleapis.com/ajax/libs/jqueryui/1.10.3/jquery-ui.min.js\"></script>\n";
 	print "</head>\n";
 	print "<body>\n";
 	print "\t<div class=\"container\">\n";
 }
 
+function KIGProjectHeaderNoBS() {
+	print "<!DOCTYPE html>\n";
+	print "<html lang=\"en\">\n";
+	print "<head>\n";
+	print "\t<meta charset=\"UTF-8\">\n";
+	print "\t<meta name=\"viewport\" content=\"width=device-width, initial-scale=1.0\">\n";
+	print "\t<title>KIG Prototype Page</title>\n";
+	print "\t<link rel=\"stylesheet\" type=\"text/css\" href=\"css/custom.css\">\n";
+	print "\t<script src=\"http://code.jquery.com/jquery-1.10.1.min.js\"></script>\n";
+	print "</head>\n";
+	print "<body>\n";
+}
+
 function KIGProjectFooter() {
 	print "\t</div>\n";
 	print "\t<script src=\"../../js/bootstrap.min.js\"></script>\n";
+	print "</body>\n";
+	print "</html>";
+}
+
+function KIGProjectFooterNoBS() {
 	print "</body>\n";
 	print "</html>";
 }
